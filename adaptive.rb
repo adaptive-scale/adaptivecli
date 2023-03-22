f25bb69661a0048e5829ba8d3773edc7f5298502
Neeraj Nabar <120240469+neerajnabar-adaptive@users.noreply.github.com>
2023-03-22 23:46:31 +0530

    Adaptive v0.2.36-staging

--- adaptive.rb
+++ adaptive.rb
@@ -4,20 +4,20 @@
 class Adaptive < Formula
   desc "Adaptive streamlines your infrastructure access and authorization"
   homepage "https://github.com/adaptive-scale/adaptivecli"
-  version "0.2.32"
+  version "v0.2.36-staging"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://cli.adaptive.live/release/adaptive-macos-i386-0.2.32.tar.gz"
-      sha256 "7c4caf2b1d9b65c952b360ca384fb2906e0c727ee3534fe81a0f7463cfcfd977"
+      url "https://cli.adaptive.live/release/adaptive-macos-amd64-$version.tar.gz"
+      sha256 "23ba464b03d0b11d0843c41c1ead1eb18e67bda909ed79154a2311b2eef2e683"
 
       def install
         bin.install "adaptive"
       end
     end
     if Hardware::CPU.arm?
-      url "https://cli.adaptive.live/release/adaptive_macos_arm64_0.2.32.tar.gz"
-      sha256 "b1350d649f446e6f3171dc636beb2d10093876eb3b1789871dee8286c1727aba"
+      url "https://cli.adaptive.live/release/adaptive_macos_arm64_$version.tar.gz"
+      sha256 "87b3993d71dec196faf25d17cbce87ee9b3dfdb595bbbb494313075f3b04144a"
 
       def install
         bin.install "adaptive"
@@ -25,22 +25,23 @@ class Adaptive < Formula
     end
   end
 
-  # on_linux do
-  #   if Hardware::CPU.intel?
-  #     url ""
-  #     sha256 ""
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://cli.adaptive.live/release/adaptive-linux-amd64-$version.tar.gz"
+      sha256 "b5993966f6e4cfc6a61bbd78c7c2ac3cc780fe68fbab297268689d8f7fa74025"
 
-  #     def install
-  #       bin.install "adaptive"
-  #     end
-  #   end
-  #   if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-  #     url ""
-  #     sha256 ""
 
-  #     def install
-  #       bin.install "adaptive"
-  #     end
-  #   end
-  # end
+      def install
+        bin.install "adaptive"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://cli.adaptive.live/release/adaptive-linux-arm64-$version.tar.gz"
+      sha256 "a2740c4e3ca5ddea4cf1ac169d9bf4839f742ce117ad65c2c7c4a46ced28b795"
+
+      def install
+        bin.install "adaptive"
+      end
+   end
+  end
 end
