8feb5c50c0f570e99ee7d06b97ca454651a8246b
Debarshi Basak <debarshi@adaptive.live>
2025-02-19 12:45:25 +0100

    Update adaptive.rb

--- adaptive.rb
+++ adaptive.rb
@@ -4,20 +4,20 @@
 class Adaptive < Formula
   desc "Adaptive streamlines your infrastructure access and authorization"
   homepage "https://github.com/adaptive-scale/adaptivecli"
-  version "v0.4.9-beta3"
+  version "v0.4.9-beta4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://cli.adaptive.live/v0.4.9-beta3/adaptive-macos-amd64-v0.4.9-beta3.tar.gz"
-      sha256 "11d3949ff30d23b937252b07f629151fa435ddea3804b0cd307ee758b2d9b5d0"
+      url "https://cli.adaptive.live/v0.4.9-beta4/adaptive-macos-amd64-v0.4.9-beta4.tar.gz"
+      sha256 "0067b790373b0282f98fd147b8e6e971d242ab3d66c6a5869e179783679eb837"
 
       def install
         bin.install "adaptive"
       end
     end
     if Hardware::CPU.arm?
-      url "https://cli.adaptive.live/v0.4.9-beta3/adaptive-macos-arm64-v0.4.9-beta3.tar.gz"
-      sha256 "c787fc23431e32e48c7e72c2cd58f409071736babfa15cdbbec843c95dbd07ce"
+      url "https://cli.adaptive.live/v0.4.9-beta4/adaptive-macos-arm64-v0.4.9-beta4.tar.gz"
+      sha256 "18d918b1df5aa537cc122e50435ec0233f12606e98662c7b11afed94ccdf71f8"
 
       def install
         bin.install "adaptive"
@@ -27,8 +27,8 @@ class Adaptive < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://cli.adaptive.live/v0.4.9-beta3/adaptive-linux-amd64-v0.4.9-beta3.tar.gz"
-      sha256 "59c6555e207e46b29d2caaa2238e65743f6fc9e47e8f1e28b7b657b4451d657b"
+      url "https://cli.adaptive.live/v0.4.9-beta4/adaptive-linux-amd64-v0.4.9-beta4.tar.gz"
+      sha256 "1b6145b424966b8bd11221456c8fb13cb08225a3889edecd405f5c739df2d30b"
 
 
       def install
@@ -37,7 +37,7 @@ class Adaptive < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://cli.adaptive.live/release/adaptive-linux-arm64-$version.tar.gz"
-      sha256 "ef3ca0f6cdb526c4c6216db2fa438197296c8a54b21cc72cb1b76c3419e180c5"
+      sha256 "30d782ba2232f883dd96b0bddbcf6c335778d7dc88172064ea8abba36a1e22b4"
 
       def install
         bin.install "adaptive"
