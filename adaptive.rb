a7a76fb8b0c0e33d01345c64167c5d1986e5557d
Debarshi Basak <debarshi@adaptive.live>
2025-12-05 11:24:36 +0530

    Update adaptive.rb

--- adaptive.rb
+++ adaptive.rb
@@ -4,20 +4,20 @@
 class Adaptive < Formula
   desc "Adaptive streamlines your infrastructure access and authorization"
   homepage "https://github.com/adaptive-scale/adaptivecli"
-  version "v0.5.0"
+  version "v0.5.7"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://cli.adaptive.live/v0.5.0/adaptive-macos-amd64-v0.5.0.tar.gz"
-      sha256 "4ba16f4d83a96ea020b8dc842b755242be6abc64898334793a2ce0e5cae805bd"
+      url "https://cli.adaptive.live/v0.5.7/adaptive-macos-amd64-v0.5.7.tar.gz"
+      sha256 "7bf3cd8aad1b83a7f0109476f1f1632410fdf310301e73f116173c3e2df200fb"
 
       def install
         bin.install "adaptive"
       end
     end
     if Hardware::CPU.arm?
-      url "https://cli.adaptive.live/v0.5.0/adaptive-macos-arm64-v0.5.0.tar.gz"
-      sha256 "4dbbbdfd4be884030b37856ddfa149cf12fd27d6f4d25ee3b03dc45c29b13b07"
+      url "https://cli.adaptive.live/v0.5.7/adaptive-macos-arm64-v0.5.7.tar.gz"
+      sha256 "45e63982ffd06c17c0f6035e83abbf7d3c01d2885863ba5ff1d642a7b82b1526"
 
       def install
         bin.install "adaptive"
@@ -27,8 +27,8 @@ class Adaptive < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://cli.adaptive.live/v0.5.0/adaptive-linux-amd64-v0.5.0.tar.gz"
-      sha256 "83a5b24f1b73565392beed4e6e37406d4e32618abec0ff4b20759c6911843ee7"
+      url "https://cli.adaptive.live/v0.5.7/adaptive-linux-amd64-v0.5.7.tar.gz"
+      sha256 "f1739ed459d42f36539826e0e33dfe3cbb45d53e25d3be335b946bcb12305bb8"
 
 
       def install
@@ -37,7 +37,7 @@ class Adaptive < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://cli.adaptive.live/release/adaptive-linux-arm64-$version.tar.gz"
-      sha256 "d7c10e1b9b066fcc3a76ca15bd7900de72ff6f04e86e011c06c86192e168b957"
+      sha256 "f1ad1479a4d5b1087522559c589e53471e75a3abfad852c55c378b5ffda7df22"
 
       def install
         bin.install "adaptive"
