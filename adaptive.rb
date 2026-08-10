b72c1eee9410b005a5c16f635b3e6ce1066fc9b0
Debarshi Basak <debarshi@adaptive.live>
2026-08-10 13:13:29 +0200

    Update SHA256 checksums for adaptive binaries

--- adaptive.rb
+++ adaptive.rb
@@ -9,7 +9,7 @@ class Adaptive < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://cli.adaptive.live/v0.7.2/adaptive-macos-amd64-v0.7.2.tar.gz"
-      sha256 "02fd20a79dcdf7e56188c784e9b044dea3fe6d3af6baa6ef272da09eeb9e34ea"
+      sha256 "8ea4cc75bdb30121f829999560d15bfc606b5bc2256db5a0d11881ebc5489e86"
 
       def install
         bin.install "adaptive"
@@ -17,7 +17,7 @@ class Adaptive < Formula
     end
     if Hardware::CPU.arm?
       url "https://cli.adaptive.live/v0.7.2/adaptive-macos-arm64-v0.7.2.tar.gz"
-      sha256 "6e473515c39391fa566139698fb8f44968871da91cb891693d778c7cdf0e5a79"
+      sha256 "fd55f30f3a3b7ffc1bf4c351ef28489ffc1ccde92dac374f37765786286d2e7e"
 
       def install
         bin.install "adaptive"
@@ -28,7 +28,7 @@ class Adaptive < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://cli.adaptive.live/v0.7.2/adaptive-linux-amd64-v0.7.2.tar.gz"
-      sha256 "f672301fc6d6816fa5377b30dd3d8317d7b858fc3791c99e9b5e0ad459b0533a"
+      sha256 "6adf0805317f707d78ad03d09d676e0ee036ec9c02b9ede0d29fab4c7977e399"
 
 
       def install
@@ -37,7 +37,7 @@ class Adaptive < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://cli.adaptive.live/release/adaptive-linux-arm64-$version.tar.gz"
-      sha256 "5cb12fa09478f59f08d970bf44aed3413680d7e179a1538219c4d881de6e8409"
+      sha256 "ea7df46e10f7160da1f603b595ff758b92ccec5852e3f41692457a3adffcb5eb"
 
       def install
         bin.install "adaptive"
